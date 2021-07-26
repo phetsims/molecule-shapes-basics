d9e7f8eb60ece5c9beee21c498d7144cb621f79e
Amy Rouinfar <amy.rouinfar@colorado.edu>
2021-07-25 23:39:22 -0600

    first pass at phetioFeatured overrides for https://github.com/phetsims/molecule-shapes-basics/issues/15

--- js/molecule-shapes-basics-phet-io-overrides.js
+++ js/molecule-shapes-basics-phet-io-overrides.js
@@ -1,2 +1,139 @@
 /* eslint-disable */
-window.phet.preloads.phetio.phetioElementsOverrides = {};
+window.phet.preloads.phetio.phetioElementsOverrides =
+  {
+    "moleculeShapesBasics.global.model.maxPairsProperty": {
+      "phetioFeatured": true
+    },
+    "moleculeShapesBasics.modelScreen.model": {
+      "phetioFeatured": true
+    },
+    "moleculeShapesBasics.modelScreen.model.moleculeGeometryProperty": {
+      "phetioFeatured": true
+    },
+    "moleculeShapesBasics.modelScreen.model.showBondAnglesProperty": {
+      "phetioFeatured": true
+    },
+    "moleculeShapesBasics.modelScreen.model.showMoleculeGeometryProperty": {
+      "phetioFeatured": true
+    },
+    "moleculeShapesBasics.modelScreen.view.bondingPanel.doubleBondNode.removeButton.enabledProperty": {
+      "phetioFeatured": false
+    },
+    "moleculeShapesBasics.modelScreen.view.bondingPanel.doubleBondNode.removeButton.visibleProperty": {
+      "phetioFeatured": false
+    },
+    "moleculeShapesBasics.modelScreen.view.bondingPanel.doubleBondNode.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculeShapesBasics.modelScreen.view.bondingPanel.singleBondNode.removeButton.enabledProperty": {
+      "phetioFeatured": false
+    },
+    "moleculeShapesBasics.modelScreen.view.bondingPanel.singleBondNode.removeButton.visibleProperty": {
+      "phetioFeatured": false
+    },
+    "moleculeShapesBasics.modelScreen.view.bondingPanel.singleBondNode.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculeShapesBasics.modelScreen.view.bondingPanel.titleNode.textProperty": {
+      "phetioFeatured": true
+    },
+    "moleculeShapesBasics.modelScreen.view.bondingPanel.titleNode.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculeShapesBasics.modelScreen.view.bondingPanel.tripleBondNode.removeButton.enabledProperty": {
+      "phetioFeatured": false
+    },
+    "moleculeShapesBasics.modelScreen.view.bondingPanel.tripleBondNode.removeButton.visibleProperty": {
+      "phetioFeatured": false
+    },
+    "moleculeShapesBasics.modelScreen.view.bondingPanel.tripleBondNode.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculeShapesBasics.modelScreen.view.bondingPanel.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculeShapesBasics.modelScreen.view.lonePairPanel.lonePairNode.removeButton.enabledProperty": {
+      "phetioFeatured": false
+    },
+    "moleculeShapesBasics.modelScreen.view.lonePairPanel.lonePairNode.removeButton.visibleProperty": {
+      "phetioFeatured": false
+    },
+    "moleculeShapesBasics.modelScreen.view.lonePairPanel.lonePairNode.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculeShapesBasics.modelScreen.view.moleculeNode.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculeShapesBasics.modelScreen.view.namePanel.titleNode.textProperty": {
+      "phetioFeatured": true
+    },
+    "moleculeShapesBasics.modelScreen.view.namePanel.titleNode.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculeShapesBasics.modelScreen.view.optionsPanel.showBondAnglesCheckbox.visibleProperty": {
+      "phetioFeatured": false
+    },
+    "moleculeShapesBasics.modelScreen.view.optionsPanel.titleNode.textProperty": {
+      "phetioFeatured": true
+    },
+    "moleculeShapesBasics.modelScreen.view.optionsPanel.titleNode.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculeShapesBasics.modelScreen.view.optionsPanel.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculeShapesBasics.realMoleculesScreen.model": {
+      "phetioFeatured": true
+    },
+    "moleculeShapesBasics.realMoleculesScreen.model.moleculeGeometryProperty": {
+      "phetioFeatured": true
+    },
+    "moleculeShapesBasics.realMoleculesScreen.model.realMoleculeShapeProperty": {
+      "phetioFeatured": true
+    },
+    "moleculeShapesBasics.realMoleculesScreen.model.showBondAnglesProperty": {
+      "phetioFeatured": true
+    },
+    "moleculeShapesBasics.realMoleculesScreen.model.showMoleculeGeometryProperty": {
+      "phetioFeatured": true
+    },
+    "moleculeShapesBasics.realMoleculesScreen.view.moleculeNode.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculeShapesBasics.realMoleculesScreen.view.moleculePanel.moleculeComboBox.visibleProperty": {
+      "phetioFeatured": false
+    },
+    "moleculeShapesBasics.realMoleculesScreen.view.moleculePanel.titleNode.textProperty": {
+      "phetioFeatured": true
+    },
+    "moleculeShapesBasics.realMoleculesScreen.view.moleculePanel.titleNode.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculeShapesBasics.realMoleculesScreen.view.moleculePanel.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculeShapesBasics.realMoleculesScreen.view.namePanel.moleculeGeometryCheckbox.visibleProperty": {
+      "phetioFeatured": false
+    },
+    "moleculeShapesBasics.realMoleculesScreen.view.namePanel.titleNode.textProperty": {
+      "phetioFeatured": true
+    },
+    "moleculeShapesBasics.realMoleculesScreen.view.namePanel.titleNode.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculeShapesBasics.realMoleculesScreen.view.namePanel.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculeShapesBasics.realMoleculesScreen.view.optionsPanel.showBondAnglesCheckbox.visibleProperty": {
+      "phetioFeatured": false
+    },
+    "moleculeShapesBasics.realMoleculesScreen.view.optionsPanel.titleNode.textProperty": {
+      "phetioFeatured": true
+    },
+    "moleculeShapesBasics.realMoleculesScreen.view.optionsPanel.titleNode.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculeShapesBasics.realMoleculesScreen.view.optionsPanel.visibleProperty": {
+      "phetioFeatured": true
+    }
+  };
