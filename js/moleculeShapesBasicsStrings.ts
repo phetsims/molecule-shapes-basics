8ecd0edbaf5bc147693abbaac89ccf13c7f4e8ec
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:34 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/moleculeShapesBasicsStrings.ts
+++ js/moleculeShapesBasicsStrings.ts
@@ -5,11 +5,13 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import moleculeShapesBasics from './moleculeShapesBasics.js';
 
 type StringsType = {
   'molecule-shapes-basics': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   }
 };
 
