3990036e29b6b8056bb801d1fafd218f7859a9e6
Jesse <jesse.greenberg@colorado.edu>
2022-09-01 16:29:33 -0400

    Rename general -> simulation in many places, see https://github.com/phetsims/joist/issues/838

--- js/molecule-shapes-basics-main.js
+++ js/molecule-shapes-basics-main.js
@@ -43,7 +43,7 @@ const simOptions = {
   webgl: true,
 
   preferencesModel: new PreferencesModel( {
-    generalOptions: {
+    simulationOptions: {
       customPreferences: generalCustomPreferences
     },
     visualOptions: {
